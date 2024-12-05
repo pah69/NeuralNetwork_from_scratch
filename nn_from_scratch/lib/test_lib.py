834161059500461899f240fa132d3e286e368b82
pah69 <engineerpah@gmail.com>
2024-12-05 19:38:23 +0700

    lo

--- nn_from_scratch/lib/test_lib.py
+++ nn_from_scratch/lib/test_lib.py
@@ -1,1 +1,11 @@
+import numpy as np
 print("test file for push")
+
+arr = np.array([1,2,3,4,5], dtype= int)
+
+print(arr)
+
+print("arr[2] =", arr[2])
+print("max in array", np.max(arr))
+# x = bytes(all,1)
+print("x =", x)
